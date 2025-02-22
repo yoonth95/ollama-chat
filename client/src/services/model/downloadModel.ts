d9dcc5ce733f048a706a27cc1a91357e0df2aecf
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-02-22 23:21:28 +0900

    Refactor: 모델 다운로드 fetch 수정

---
+++ client/src/services/model/downloadModel.ts
@@ -0,0 +1,74 @@
+import { ApiError } from "@/lib/fetchAndCustomError";
+import { revalidateTagAction } from "@/actions/revalidateTagAction";
+import { toast } from "react-toastify";
+import { DigestWithProgressType } from "@/store/useModelDownloadStore";
+
+export default async function downloadModel(
+  model_name: string,
+  updateProgress: (progress: DigestWithProgressType) => void,
+  resetInput?: () => void,
+) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/model/download?model_name=${encodeURIComponent(model_name)}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+
+    if (!response.ok) {
+      const result = await response.json();
+      throw new ApiError(response.status, result.message || "API 요청 오류 발생");
+    }
+
+    const reader = response.body?.getReader();
+    if (!reader) return;
+
+    const decoder = new TextDecoder();
+    let buffer = "";
+
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) break;
+
+      buffer += decoder.decode(value, { stream: true });
+
+      const lines = buffer.split("\n");
+      buffer = lines.pop() || "";
+
+      for (const line of lines) {
+        if (!line.trim()) continue;
+        try {
+          const data = JSON.parse(line);
+          const { model_name, digest, progress, status } = data.data;
+
+          if (progress !== undefined) {
+            const digestValue = digest !== undefined ? digest : "";
+            updateProgress({
+              model_name,
+              digest: digestValue,
+              progress,
+            });
+          }
+
+          if (status === "success") {
+            await revalidateTagAction("models");
+            if (resetInput) resetInput();
+            toast.success("모델 다운로드가 완료되었습니다.");
+          }
+        } catch (error) {
+          console.error("JSON 파싱 오류", error);
+          break;
+        }
+      }
+    }
+
+    console.log(model_name, "다운로드 완료!");
+  } catch (error) {
+    if (error instanceof ApiError) {
+      return { data: null, error: { status: error.status, message: error.message } };
+    }
+    return { data: null, error: { status: 500, message: "알 수 없는 에러가 발생했습니다." } };
+  }
+}

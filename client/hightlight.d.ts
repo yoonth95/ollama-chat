5b2b3160ebd3dc95086301069f389b6f32228a69
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-02-25 01:33:05 +0900

    Feat: highlight.js 타입 설정

---
+++ client/hightlight.d.ts
@@ -0,0 +1,20 @@
+declare module "highlight.js/lib/languages/typescript" {
+  import { LanguageFn } from "highlight.js";
+  const language: LanguageFn;
+  export default language;
+}
+declare module "highlight.js/lib/languages/javascript" {
+  import { LanguageFn } from "highlight.js";
+  const language: LanguageFn;
+  export default language;
+}
+declare module "highlight.js/lib/languages/xml" {
+  import { LanguageFn } from "highlight.js";
+  const language: LanguageFn;
+  export default language;
+}
+declare module "highlight.js/lib/languages/css" {
+  import { LanguageFn } from "highlight.js";
+  const language: LanguageFn;
+  export default language;
+}

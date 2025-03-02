ea4c37c19f204335776d6b0c757e1c3f25f74077
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-02 21:52:32 +0900

    Refactor: 사용하지 않는 코드 삭제

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,10 +1,6 @@
 import { ModelText, ChatInputContainer } from "@/app/(main)/_components";
 
 export default function MainPage() {
-  const handleSendMessage = (message: string) => {
-    console.log("메시지 전송:", message);
-  };
-
   return (
     <div className="m-auto w-full max-w-6xl translate-y-6 px-2 pb-24">
       <div className="flex h-full w-full flex-col items-center justify-center gap-6">

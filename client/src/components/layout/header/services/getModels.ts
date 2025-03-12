337635cb7987b87fc8621a22c6cfa8e1e783c9b8
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-12 23:38:02 +0900

    Refactor: 모델 조회 prefetch 적용

--- client/src/components/layout/header/services/getModels.ts
+++ client/src/components/layout/header/services/getModels.ts
@@ -1,10 +1,14 @@
 import { customFetch } from "@/lib/customFetch";
 import { ModelInfoArraySchema, ModelInfoType } from "@/types/modelType";
 
-export async function getModels() {
-  return customFetch<ModelInfoType[]>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/models`, ModelInfoArraySchema, {
-    next: { tags: ["models"] },
-  });
-}
+export const getModels = async () => {
+  return await customFetch<ModelInfoType[]>(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/models`,
+    ModelInfoArraySchema,
+    {
+      cache: "no-store",
+    },
+  );
+};
 
 export default getModels;

864e899f2c047a07ecc6abb66562d66c63d4ac40
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-13 23:34:05 +0900

    Refactor: database 파일 수정

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -1,24 +1,22 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-# SQLite 데이터베이스 파일 경로 설정
+# 데이터베이스 설정
 DATABASE_URL = settings.DATABASE_URL
-
-# SQLAlchemy 엔진 생성
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-# 세션 로컬 클래스 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# 베이스 클래스 선언
 Base = declarative_base()
 
+# DB 초기화
 def init_db():
-  Base.metadata.create_all(bind=engine)
+  from app.db.models.room import Room  
+  from app.db.models.chat import ChatMessage 
   
-# 의존성으로 사용할 세션 함수
+  Base.metadata.create_all(bind=engine)
+
+# 세션 생성 함수
 def get_db():
   db = SessionLocal()
   try:

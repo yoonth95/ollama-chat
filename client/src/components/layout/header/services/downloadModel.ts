5dd5240eaf2dfa000c4ace4ecfad63ceb5c23565
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-18 23:28:49 +0900

    Style: console 삭제

--- client/src/components/layout/header/services/downloadModel.ts
+++ client/src/components/layout/header/services/downloadModel.ts
@@ -66,7 +66,6 @@ const downloadModel = async ({ modelName, updateProgress }: DownloadModelPropsTy
     }
 
     // 모든 데이터를 읽었지만 `success` 상태를 받지 못한 경우
-    console.log("여기333333333333333333333333333");
     return { ok: false, message: "다운로드가 예상치 못한 상태로 종료되었습니다." };
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : "알 수 없는 오류가 발생했습니다.";

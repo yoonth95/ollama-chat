fa5c0f9d06879b69c2a06ae251a03953154ed11d
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-03-18 23:28:22 +0900

    Design: Input 컴포넌트 placeholder css 수정

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border-0 bg-white px-3 py-2 text-base text-foreground ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-neutral-950 placeholder:text-muted-foreground focus:border-transparent focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-accent dark:bg-accent dark:ring-offset-accent dark:file:text-neutral-50 dark:placeholder:text-neutral-400 dark:focus-visible:ring-0 dark:focus-visible:ring-transparent md:text-sm",
+          "placeholder:text-placeholder dark:placeholder:text-placeholder flex h-10 w-full rounded-md border-0 bg-white px-3 py-2 text-base text-foreground ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-neutral-950 focus:border-transparent focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-accent dark:bg-accent dark:ring-offset-accent dark:file:text-neutral-50 dark:focus-visible:ring-0 dark:focus-visible:ring-transparent md:text-sm",
           className,
         )}
         ref={ref}
